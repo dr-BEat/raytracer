e831b79dab1b1042c991bbda4d8605d9ad75a692
dr-BEat <dr-BEat@users.noreply.github.com>
2020-11-28 19:17:11 +0100

    Create Bmp

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 
+#[macro_use]
+extern crate bmp;
+use bmp::{Image, Pixel};
+
 const IMAGE_WIDTH:u32 = 256;
 const IMAGE_HEIGHT:u32 = 256;
 
 fn main() {
+    let mut img = Image::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
-    
-    for j in (0..IMAGE_HEIGHT).rev() {
-        for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH-1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT-1) as f64;
-            let b = 0.25;
-
-            let ir = (255.999 * r) as u32;
-            let ig = (255.999 * g) as u32;
-            let ib = (255.999 * b) as u32;
-
-            println!("{} {} {}", ir, ig, ib);
-        }
+    for (x, y) in img.coordinates() {
+        img.set_pixel(x, y, px!(x, y, 200));
     }
+    let _ = img.save("img.bmp");
+    println!("Created img.bmp");
 }
