fb00e86e14b3c1db67f7ea7ea229eb40f49b0913
dr-BEat <dr-BEat@users.noreply.github.com>
2020-11-28 21:39:16 +0100

    Sphere surface normals

--- src/main.rs
+++ src/main.rs
@@ -33,24 +33,30 @@ fn pixel_from_color(color: Color) -> Pixel {
     )
 }
 
-fn hit_sphere(center: &Point, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin - *center;
     let a = r.direction.dot(r.direction);
     let b = 2.0 * oc.dot(r.direction);
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point::from_array([0.0, 0.0, 1.0]), 0.5, &r) {
-        return Color::from_array([1.0, 0.0, 0.0]);
+    let t = hit_sphere(&Point::from_array([0.0, 0.0, -1.0]), 0.5, &r);
+    if t > 0.0 {
+        let n = (r.at(t) - Vector::from_array([0.0, 0.0, -1.0])).normalize();
+        return 0.5 * Color::from_array([n[0] + 1.0, n[1] + 1.0, n[2] + 1.0]);
     }
 
     // Background
     let unit_direction = r.direction.normalize();
     let t = 0.5 * (unit_direction[1] + 1.0);
-    (1.0 - t) * Color::from_array([0.5, 0.7, 1.0]) + t * Color::from_array([1.0, 1.0, 1.0])
+    (1.0 - t) * Color::from_array([1.0, 1.0, 1.0]) + t * Color::from_array([0.5, 0.7, 1.0])
 }
 
 fn main() {
@@ -75,13 +81,10 @@ fn main() {
 
     for (x, y) in img.coordinates() {
         if x == 0 {
-            print!(
-                "\rScanlines remaining: {:>3}%",
-                100 - 100 * y / image_height
-            );
+            print!("\rScanlines remaining: {:>3}%", 100 * y / image_height);
         }
         let u = x as f64 / (image_width - 1) as f64;
-        let v = y as f64 / (image_height - 1) as f64;
+        let v = (image_height - y - 1) as f64 / (image_height - 1) as f64;
         let r = Ray::new(
             origin,
             lower_left_corner + u * horizontal + v * vertical - origin,
