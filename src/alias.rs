0a3c32be81042146fde9f004ba45d35e053a17c9
dr-BEat <dr-BEat@users.noreply.github.com>
2020-11-30 19:09:32 +0100

    Fixed bug in random_in_unit_sphere method

--- src/alias.rs
+++ src/alias.rs
@@ -8,7 +8,11 @@ pub type Vector = Vec3<f64>;
 pub fn random_in_unit_sphere() -> Point {
     let mut rng = rand::thread_rng();
     loop {
-        let p = Point::from_array([rng.gen(), rng.gen(), rng.gen()]);
+        let p = Point::from_array([
+            rng.gen_range(-1.0, 1.0),
+            rng.gen_range(-1.0, 1.0),
+            rng.gen_range(-1.0, 1.0),
+        ]);
 
         if p.sqrlen() < 1.0 {
             return p;
