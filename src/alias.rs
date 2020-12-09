cb656dcb855d57d9bab70fc4bd9464bb73fefc7d
dr-BEat <dr-BEat@users.noreply.github.com>
2020-12-09 22:34:22 +0100

    Added quaternion rotation

--- src/alias.rs
+++ src/alias.rs
@@ -4,6 +4,7 @@ pub use vecmat::vec::*;
 pub type Color = Vec3<f64>;
 pub type Point = Vec3<f64>;
 pub type Vector = Vec3<f64>;
+pub type Quaternion = Vec4<f64>;
 
 pub trait Vec3Ext<T>
 where
@@ -16,6 +17,7 @@ where
     fn near_zero(&self) -> bool;
     fn reflect(&self, n: &Vector) -> Vector;
     fn refract(&self, n: &Vector, etai_over_etat: f64) -> Vector;
+    fn rotate(&self, q: &Quaternion) -> Vector;
 }
 
 impl Vec3Ext<f64> for Vec3<f64> {
@@ -66,5 +68,76 @@ impl Vec3Ext<f64> for Vec3<f64> {
         let r_out_perp = etai_over_etat * (*self + cos_theta * *n);
         let r_out_parallel = -(1.0 - r_out_perp.sqrlen()).abs().sqrt() * *n;
         r_out_perp + r_out_parallel
+    }
+
+    fn rotate(&self, q: &Quaternion) -> Vector {
+        let p = Vec4::<f64>::from_array([0.0, self[0], self[1], self[2]]);
+        println!("p: {} q: {}", p, q);
+        let result = q.hamiltonian_prod(&p);
+        println!("r: {}", result);
+        let inv_q = Quaternion::from_array([q[0], -q[1], -q[2], -q[3]]);
+        let result = result.hamiltonian_prod(&inv_q);
+        println!("r: {}", result);
+        Vector::from_array([result[1], result[2], result[3]])
+    }
+}
+
+pub trait Vec4Ext<T>
+where
+    T: Copy + Default,
+{
+    fn new_quaternion(angle: f64, v: Vec3<T>) -> Vec4<T>;
+    fn hamiltonian_prod(&self, other: &Vec4<T>) -> Vec4<T>;
+}
+
+impl Vec4Ext<f64> for Vec4<f64> {
+    fn new_quaternion(angle: f64, v: Vector) -> Quaternion {
+        let v = v.normalize() * (angle / 2.0).sin();
+        Quaternion::from_array([(angle / 2.0).cos(), v[0], v[1], v[2]])
+    }
+
+    fn hamiltonian_prod(&self, other: &Quaternion) -> Quaternion {
+        Quaternion::from_array([
+            self[0] * other[0] - self[1] * other[1] - self[2] * other[2] - self[3] * other[3],
+            self[0] * other[1] + self[1] * other[0] + self[2] * other[3] - self[3] * other[2],
+            self[0] * other[2] - self[1] * other[3] + self[2] * other[0] + self[3] * other[1],
+            self[0] * other[3] + self[1] * other[2] - self[2] * other[1] + self[3] * other[0],
+        ])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test]
+    fn test_rotate() {
+        let q =
+            Quaternion::new_quaternion(90.0f64.to_radians(), Vector::from_array([0.0, 1.0, 0.0]));
+        let p = Vector::from_array([1.0, 0.0, 0.0]);
+        assert!((p.rotate(&q) - Vector::from_array([0.0, 0.0, -1.0])).near_zero());
+    }
+
+    #[test_case( 90.0, Vector::from_array([0.0, 1.0, 0.0]), Vector::from_array([0.0, 0.0, -1.0]))]
+    #[test_case( 180.0, Vector::from_array([0.0, 1.0, 0.0]), Vector::from_array([-1.0, 0.0, 0.0]))]
+    #[test_case( 270.0, Vector::from_array([0.0, 1.0, 0.0]), Vector::from_array([0.0, 0.0, 1.0]))]
+    #[test_case( 45.0, Vector::from_array([0.0, 1.0, 0.0]), Vector::from_array([0.7071067811865475, 0.0, -0.7071067811865476]))]
+    fn test_rotations(angle: f64, axis: Vector, result: Vector) {
+        let q = Quaternion::new_quaternion(angle.to_radians(), axis);
+        let p = Vector::from_array([1.0, 0.0, 0.0]);
+        assert!((p.rotate(&q) - result).near_zero());
+    }
+
+    use proptest::prelude::*;
+    proptest! {
+
+        #[test]
+        fn test_rotate_around_same_axis_does_nothing(angle in 0.0f64..std::f64::consts::PI) {
+            let q =
+                Quaternion::new_quaternion(angle, Vector::from_array([0.0, 1.0, 0.0]));
+            let p = Vector::from_array([0.0, 1.0, 0.0]);
+            prop_assert!((p.rotate(&q) - p).near_zero());
+        }
     }
 }
